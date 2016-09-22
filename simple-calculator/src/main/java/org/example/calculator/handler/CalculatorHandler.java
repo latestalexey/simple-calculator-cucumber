34069a980d7d372ead46dafa5d3e3b2de467c013
Shane Rowley <shanemrowley@gmail.com>
2016-09-22 10:56:19 -0400

    clean up for project start

--- simple-calculator/src/main/java/org/example/calculator/handler/CalculatorHandler.java
+++ simple-calculator/src/main/java/org/example/calculator/handler/CalculatorHandler.java
@@ -3,23 +3,19 @@ package org.example.calculator.handler;
 public class CalculatorHandler {
 
 	public int doAddition(int operand1, int operand2) {
-		int result = operand1 + operand2;
-		return result;
+		return operand1 + operand2;
 	}
 
 	public int doSubtraction(int operand1, int operand2) {
-		int result = operand1 - operand2;
-		return result;
+		return operand1 - operand2;
 	}
 
 	public int doMultiplication(int operand1, int operand2) {
-		int result = operand1 * operand2;
-		return result;
+		return operand1 * operand2;
 	}
 
 	public int doDivision(int operand1, int operand2) {
-		int result = operand1 / operand2;
-		return result;
+		return operand1 / operand2;
 	}
 
 }
