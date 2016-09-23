c5c7062dd24985a7bd7b459706e7d97f05944e56
Shane Rowley <shanemrowley@gmail.com>
2016-09-23 01:36:43 -0400

    updated enum

--- simple-calculator/src/main/java/org/example/calculator/handler/Operator.java
+++ simple-calculator/src/main/java/org/example/calculator/handler/Operator.java
@@ -1,28 +1,8 @@
 package org.example.calculator.handler;
 
 public enum Operator {
-	ADDITION("an addition"),
-	SUBTRACTION("a subtraction"),
-	MULTIPLICATION("a multiplication"),
-	DIVISION("a division");
-	
-	private String desc;
-	
-	private Operator(String desc) {
-		this.desc = desc;
-	}
-	
-	public String getDesc() {
-		return desc;
-	}
-	
-	public static Operator getByDesc(String desc) {
-		for (Operator operator: Operator.values()) {
-			if (operator.getDesc().equalsIgnoreCase(desc)) {
-				return operator;
-			}
-		}
-			
-		return null;
-	}
+	ADDITION,
+	SUBTRACTION,
+	MULTIPLICATION,
+	DIVISION;
 }
